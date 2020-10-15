a22b6d67a0b6919e73d3a56a657556d29ba2abf2
tzvaita <tzvaita@gmail.com>
2020-10-15 19:32:01 +0200

    Render buttons to view

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
+import Display from './display';
+import ButtonCont from './buttonCont';
 
 function App() {
   return (
     <div>
       <h1> Hello and welcome to my React calculator </h1>
+      <Display />
+      <ButtonCont />
     </div>
   );
 }
---
+++ src/components/buttonCont.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Button from './button';
+
+export default function ButtonCont() {
+  const grp1 = ['AC', '+/-', '%', '÷'];
+  const grp2 = ['7', '8', '9', 'X'];
+  const grp3 = ['4', '5', '6', '-'];
+  const grp4 = ['1', '2', '3', '+'];
+  const grp5 = ['0', '.', '='];
+
+  const grps = [grp1, grp2, grp3, grp4, grp5];
+
+  return (
+    <div className="btnPanel">
+      {grps.map(grp => (
+        <div className="grp" key={grp}>
+          {grp.map(char => (
+            <Button key={char} btnName={`${char}`} />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
